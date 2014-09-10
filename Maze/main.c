72431073305527035d16ae12e94d9ac728ff5872
3axap <3axap123@mail.ru>
2014-09-10 19:24:16 +0300

    allocate function

--- Maze/main.c
+++ Maze/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void allocateMatrix(int ***matrix, int width, int height);
+
 int main()
 {
-    printf("Hello world!\n");
+    int **maze = NULL, width = 30, height = 20;
+    int **fog = NULL;
+
     return 0;
+}
+
+void allocateMatrix(int ***matrix, int width, int height)
+{
+    *matrix = (int **) malloc(height * sizeof(int *));
+
+    for(int i = 0; i < height; ++i)
+    {
+        (*matrix)[i] = NULL;
+    }
+
+    for(int i = 0; i < height; ++i)
+    {
+        (*matrix)[i] = (int *) malloc(width * sizeof(int));
+    }
 }
