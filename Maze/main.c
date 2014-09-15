110bdf6519ff74c1ba3d40884c938ff0b3023c27
DimaShag <aaskks@ya.ru>
2014-09-15 19:38:55 +0300

    struct refactoring

--- Maze/main.c
+++ Maze/main.c
@@ -3,28 +3,42 @@
 #include <time.h>
 #include "compatibility.h"
 
-void allocateMatrix(int ***matrix, int width, int height);
-void freeMatrix(int ***matrix, int width, int height);
-void drawMaze(int **maze, int **fog, int width, int height, int heroX, int heroY);
-void generateMaze(int **maze, int width, int height, int *heroX, int *heroY);
-void generateFog(int **fog, int width, int height, int heroX, int heroY);
+typedef struct _Position
+{
+    int x, y;
+} Position;
+
+typedef struct _Matrix
+{
+    int **matrix;
+    int width;
+    int height;
+} Matrix;
+
+void allocateMatrix(Matrix *a, int width, int height);
+void freeMatrix(Matrix *a);
+void drawMaze(Matrix maze, Matrix fog, Position hero);
+void generateMaze(Matrix maze, Position *hero);
+void generateFog(Matrix fog, Position hero);
 
 int main()
 {
     srand(time(NULL));
-    int **maze = NULL, width = 30, height = 20;
-    int **fog = NULL;
-    int heroX = 5, heroY = 7;
+    Matrix maze={NULL,0,0};
+    Matrix fog = {NULL,0,0};
+    int width = 30, height = 20;
+    Position hero;
+
 
     allocateMatrix(&maze, width, height);
     allocateMatrix(&fog, width, height);
 
-    generateMaze(maze, width, height, &heroX, &heroY);
-    generateFog(fog, width, height, heroX, heroY);
-    drawMaze(maze, fog, width, height, heroX, heroY);
+    generateMaze(maze, &hero);
+    generateFog(fog, hero);
+    drawMaze(maze, fog, hero);
 
-    freeMatrix(&fog, width, height);
-    freeMatrix(&maze, width, height);
+    freeMatrix(&fog);
+    freeMatrix(&maze);
 
 
     return 0;
@@ -108,35 +122,39 @@ void generateMaze(int **maze, int width, int height, int *heroX, int *heroY)
         maze[height - 1][i] = 1;
     }
 
-    int perimetr = 2 * (width -2) + 2 * (height - 2);
+    int perimetr = 2 * (width - 2) + 2 * (height - 2);
     int exit = rand() % perimetr;
 
-    if(exit < width-2)
-        maze[0][exit+1] = 0;
-    else if(exit < 2*(width-2))
-        maze[height-1][exit-width+3] = 0;
-    else if(exit<2*(width-2)+height-2)
-        maze[exit-2*width+5][0]=0;
+    if(exit < width - 2)
+        maze[0][exit + 1] = 0;
+    else if(exit < 2 * (width - 2))
+        maze[height - 1][exit - width + 3] = 0;
+    else if(exit < 2 * (width - 2) + height - 2)
+        maze[exit - 2 * width + 5][0] = 0;
     else
-        maze[exit-2*width-height+7][width-1]=0;
+        maze[exit - 2 * width - height + 7][width - 1] = 0;
 
-    int countOfWall=(width-2)*(height-2)/2;
-    for (int i = 0; i < countOfWall; ++i)
+    int countOfWall = (width - 2) * (height - 2) / 2;
+
+    for(int i = 0; i < countOfWall; ++i)
     {
-        int wallX=rand()%(width-2)+1;
-        int wallY=rand()%(height-2)+1;
-        maze[wallY][wallX]=1;
+        int wallX = rand() % (width - 2) + 1;
+        int wallY = rand() % (height - 2) + 1;
+        maze[wallY][wallX] = 1;
     }
-    int flag=1;
-    int x,y;
+
+    int flag = 1;
+    int x, y;
+
     do
     {
-        x=rand()%(width-2)+1;
-        y=rand()%(height-2)+1;
-        flag=maze[y][x]!=0;
-    }while(flag);
-    *heroX=x;
-    *heroY=y;
+        x = rand() % (width - 2) + 1;
+        y = rand() % (height - 2) + 1;
+        flag = maze[y][x] != 0;
+    } while(flag);
+
+    *heroX = x;
+    *heroY = y;
 }
 
 void generateFog(int **fog, int width, int height, int heroX, int heroY)
