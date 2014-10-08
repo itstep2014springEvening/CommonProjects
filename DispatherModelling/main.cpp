7cc03f7138e8763e367ced07ab507121def5de1a
Maixi <www.vacek95@mail.ru>
2014-10-08 20:22:01 +0000

    first part

--- DispatherModelling/main.cpp
+++ DispatherModelling/main.cpp
@@ -1,11 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <exception>
 
+struct Process
+{
+	int id, needTime, workedTime, waitedTime;
+	enum Status {wait, active} status;
+} ;
 
+struct Node
+{
+	Process process ;
+	Node *next, *previous;
+};
 
+void push(Node *&he200ad, Process process);
 
+void pop(Node *&head);
+
+Process onHead(Node *head);
+
+void roundRobin(Node *&head);
+
+bool applyOnHead(Node *head, bool (*functor)(Process &));
+
+void applyOnRest(Node *head, bool (*functor)(Process &));
+
+bool work(Process 200 & process);
+
+bool wait(Process &process);
 
 int main()
-
 {
-    return 0;
+	return 0;
+}
+
+bool work(Process &process)
+{
+	process.status = active;
+	++process.workedTime;
+	return process.needTime == process.workedTime;
+}
+
+bool wait(Process &process)
+{
+	process.status = wait;
+	++process.waitedTime;
+	return false;
+}
+void push(Node *&head, Process process);
+
+void pop(Node *&head);
+
+Process onHead(Node *head)
+{
+	if(head == nullptr)
+		throw std::exception();
+
+	return head->process;
 }
