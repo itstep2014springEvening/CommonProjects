ab41eca5a11f229e5419f4aff44ffe676d0b4271
Harald Zealot <harald_zealot@tut.by>
2014-10-09 20:22:09 +0300

    parametridized

--- DispatherModelling/main.cpp
+++ DispatherModelling/main.cpp
@@ -15,232 +15,230 @@ struct Node
     Node *next, *previous;
 };
 
-if(index == 1)
+
+void push(Node *&head, Process process);
+
+void pop(Node *&head);
+
+Process onHead(Node *head);
+
+void roundRobin(Node *&head);
+
+bool applyOnHead(Node *head, bool (*functor)(Process &));
+
+void applyOnRest(Node *head, bool (*functor)(Process &));
+
+bool work(Process &process);
+
+bool wait(Process &process);
+
+int find(int *startTime, int size, int value);
+
+enum Algorithm {fcfs, rr};
+
+int main(int argc, char **argv)
 {
-    obj.tail->next = head;
-    void push(Node *&head, Process process);
-
-    void pop(Node *&head);
-
-    Process onHead(Node * head);
-
-    void roundRobin(Node *&head);
-
-    bool applyOnHead(Node * head, bool (*functor)(Process &));
-
-    void applyOnRest(Node * head, bool (*functor)(Process &));
-
-    bool work(Process & process);
-
-    bool wait(Process & process);
-
-    int find(int *startTime, int size, int value);
-
-    enum Algorithm {fcfs, rr};
-
-    int main(int argc, char **argv)
+    if(argc < 2 || argc > 3)
     {
-        if(argc < 2 || argc > 3)
+        printf("Wrong usage. 2 or 3 arguments\n");
+        return 1;
+    }
+
+    Algorithm algorithm;
+    int rrTurn;
+
+    if(strcmp("fcfs", argv[1]) == 0)
+        algorithm = fcfs;
+    else if(strcmp("rr", argv[1]) == 0)
+    {
+        if(argc != 3)
         {
-            printf("Wrong usage. 2 or 3 arguments\n");
+            printf("Wrong usage. Round robin must have count of quantums\n");
             return 1;
         }
 
-        Algorithm algorithm;
-        int rrTurn;
+        algorithm = rr;
+        rrTurn = atoi(argv[2]);
 
-        if(strcmp("fcfs", argv[1]) == 0)
-            algorithm = fcfs;
-        else if(strcmp("rr", argv[1]) == 0)
+        if(rrTurn <= 0)
         {
-            if(argc != 3)
-            {
-                printf("Wrong usage. Round robin must have count of quantums\n");
-                return 1;
-            }
-
-            algorithm = rr;
-            rrTurn = atoi(argv[2]);
-
-            if(rrTurn <= 0)
-            {
-                printf("Third parameter must be positive integer, stupid!!!");
-                return 2;
-            }
-        }
-
-        int *startTime = nullptr;
-        Process *array = nullptr;
-        int n;
-        Node *head = nullptr;
-        scanf("%d", &n);
-        startTime = new int [n];
-        array = new Process[n];
-
-        for(int i = 0; i < n; ++i)
-        {
-            scanf("%d", &startTime[i]);
-            scanf("%d", &array[i].needTime);
-            array[i].id = i;
-            array[i].workedTime = 0;
-            array[i].waitedTime = 0;
-            array[i].status = Process::Status::WAIT;
-        }
-
-        int currentQuantum = 0, countOfTerminatedProcesses = 0;
-        Process *result = nullptr;
-        result = new Process[n];
-
-        while(countOfTerminatedProcesses != n)
-        {
-            int index = find(startTime, n, currentQuantum);
-
-            if(index >= 0)
-                push(head, array[index]);
-
-            applyOnRest(head, wait);
-
-            if(applyOnHead(head, work))
-            {
-                result[countOfTerminatedProcesses] = onHead(head);
-                ++countOfTerminatedProcesses;
-                pop(head);
-            }
-
-            if(algorithm == rr && currentQuantum % rrTurn == 0)
-            {
-                roundRobin(head);
-            }
-
-            ++currentQuantum;
-        }
-
-        for(int i = 0; i < n; ++i)
-        {
-            printf("id %d", result[i].id);
-            printf("\t LT=%d", result[i].workedTime + result[i].waitedTime);
-            printf("\t ET=%d", result[i].workedTime);
-            printf("\t WT=%d", result[i].waitedTime);
-            printf("\t eff-s=%0.3f\n", (double)result[i].workedTime / (result[i].workedTime + result[i].waitedTime));
-        }
-
-        delete [] result;
-        delete [] array;
-        delete [] startTime;
-        return 0;
-    }
-
-    int find(int *startTime, int size, int value)
-    {
-        int result = -1;
-
-        for(int i = 0; i < size; ++i)
-        {
-            if(startTime [i] == value)
-                return i;
-        }
-
-        return result;
-    }
-
-
-    bool work(Process & process)
-    {
-        process.status = Process::ACTIVE;
-        ++process.workedTime;
-        return process.needTime == process.workedTime;
-    }
-
-    bool wait(Process & process)
-    {
-        process.status = Process::WAIT;
-        ++process.waitedTime;
-        return false;
-    }
-
-    void push(Node *&head, Process process)
-    {
-        Node *p = nullptr;
-        p = new Node;
-        p->process = process;
-
-        if(head == nullptr)
-        {
-            p->next = p;
-            p->previous = p;
-            head = p;
-        }
-        else
-        {
-            p->next = head;
-            p->previous = head->previous;
-            head->previous->next = p;
-            head->previous = p;
-            /*
-            Node *tail=head->previous;
-            p->next=head;
-            p->previous=tail;
-            tail->next=p;
-            head->previous=p;
-            */
+            printf("Third parameter must be positive integer, stupid!!!");
+            return 2;
         }
     }
 
-    void pop(Node *&head)
-    {
-        if(head == nullptr)
-            return;
+    int *startTime = nullptr;
+    Process *array = nullptr;
+    int n;
+    Node *head = nullptr;
+    scanf("%d", &n);
+    startTime = new int [n];
+    array = new Process[n];
 
-        if(head->next == head)
-        {
-            delete head;
-            head = nullptr;
-        }
-        else
-        {
-            Node *p = head;
-            head = head->next;
-            p->previous->next = head;
-            head->previous = p->previous;
-            delete p;
-            p = nullptr;
-        }
+    for(int i = 0; i < n; ++i)
+    {
+        scanf("%d", &startTime[i]);
+        scanf("%d", &array[i].needTime);
+        array[i].id = i;
+        array[i].workedTime = 0;
+        array[i].waitedTime = 0;
+        array[i].status = Process::Status::WAIT;
     }
 
-    Process onHead(Node * head)
-    {
-        if(head == nullptr)
-            throw std::exception();
+    int currentQuantum = 0, countOfTerminatedProcesses = 0;
+    Process *result = nullptr;
+    result = new Process[n];
 
-        return head->process;
+    while(countOfTerminatedProcesses != n)
+    {
+        int index = find(startTime, n, currentQuantum);
+
+        if(index >= 0)
+            push(head, array[index]);
+
+        applyOnRest(head, wait);
+
+        if(applyOnHead(head, work))
+        {
+            result[countOfTerminatedProcesses] = onHead(head);
+            ++countOfTerminatedProcesses;
+            pop(head);
+        }
+
+        if(algorithm == rr && currentQuantum % rrTurn == 0)
+        {
+            roundRobin(head);
+        }
+
+        ++currentQuantum;
     }
 
-    void roundRobin(Node *&head)
+    for(int i = 0; i < n; ++i)
     {
-        if(head == nullptr)
-            return;
+        printf("id %d", result[i].id);
+        printf("\t LT=%d", result[i].workedTime + result[i].waitedTime);
+        printf("\t ET=%d", result[i].workedTime);
+        printf("\t WT=%d", result[i].waitedTime);
+        printf("\t eff-s=%0.3f\n", (double)result[i].workedTime / (result[i].workedTime + result[i].waitedTime));
+    }
 
+    delete [] result;
+    delete [] array;
+    delete [] startTime;
+    return 0;
+}
+
+int find(int *startTime, int size, int value)
+{
+    int result = -1;
+
+    for(int i = 0; i < size; ++i)
+    {
+        if(startTime [i] == value)
+            return i;
+    }
+
+    return result;
+}
+
+
+bool work(Process &process)
+{
+    process.status = Process::ACTIVE;
+    ++process.workedTime;
+    return process.needTime == process.workedTime;
+}
+
+bool wait(Process &process)
+{
+    process.status = Process::WAIT;
+    ++process.waitedTime;
+    return false;
+}
+
+void push(Node *&head, Process process)
+{
+    Node *p = nullptr;
+    p = new Node;
+    p->process = process;
+
+    if(head == nullptr)
+    {
+        p->next = p;
+        p->previous = p;
+        head = p;
+    }
+    else
+    {
+        p->next = head;
+        p->previous = head->previous;
+        head->previous->next = p;
+        head->previous = p;
+        /*
+        Node *tail=head->previous;
+        p->next=head;
+        p->previous=tail;
+        tail->next=p;
+        head->previous=p;
+        */
+    }
+}
+
+void pop(Node *&head)
+{
+    if(head == nullptr)
+        return;
+
+    if(head->next == head)
+    {
+        delete head;
+        head = nullptr;
+    }
+    else
+    {
+        Node *p = head;
         head = head->next;
+        p->previous->next = head;
+        head->previous = p->previous;
+        delete p;
+        p = nullptr;
     }
+}
 
-    bool applyOnHead(Node * head, bool (*functor)(Process &))
+Process onHead(Node *head)
+{
+    if(head == nullptr)
+        throw std::exception();
+
+    return head->process;
+}
+
+void roundRobin(Node *&head)
+{
+    if(head == nullptr)
+        return;
+
+    head = head->next;
+}
+
+bool applyOnHead(Node *head, bool (*functor)(Process &))
+{
+    if(head == nullptr)
+        throw std::exception();
+
+    return functor(head->process);
+}
+
+void applyOnRest(Node *head, bool (*functor)(Process &))
+{
+    if(head != nullptr)
     {
-        if(head == nullptr)
-            throw std::exception();
+        Node *p = head->next;
 
-        return functor(head->process);
-    }
-
-    void applyOnRest(Node * head, bool (*functor)(Process &))
-    {
-        if(head != nullptr)
+        while(p != head)
         {
-            Node *p = head->next;
-
-            while(p != head)
-            {
-                functor(p->process);
-                p = p->next;
-            }
+            functor(p->process);
+            p = p->next;
         }
     }
+}
