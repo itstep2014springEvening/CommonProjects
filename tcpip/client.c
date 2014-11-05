011524f1776556b272fbde01d02ed9075a224e39
Harald Zealot <harald_zealot@tut.by>
2014-11-05 20:47:26 +0300

    fixes

--- tcpip/client.c
+++ tcpip/client.c
@@ -7,16 +7,17 @@
 
 int main()
 {
-	struct sockaddr_in host;
-	host.sin_family = AF_INET;
-	host.sin_port = htons(7500);
-	host.sin_addr.s_addr = inet_addr("127.0.0.1");
 	int theSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if(theSocket < 0)
 	{
 		fprintf(stderr,"ошибка вызова функции socket\n");
 		exit(1);
 	}
+
+	struct sockaddr_in host;
+	host.sin_family = AF_INET;
+	host.sin_port = htons(7500);
+	host.sin_addr.s_addr = inet_addr("127.0.0.1");
 	int status;
 	status = connect(theSocket, (struct sockaddr *) &host, sizeof(host));
 	if(status)
@@ -24,12 +25,14 @@ int main()
 		fprintf(stderr, "ошибка вызова функции connect");
 		exit(1);
 	}
+
 	status = write(theSocket, "a", 1);
 	if(status <= 0)
 	{
 		fprintf(stderr, "ошибка вызова write");
 		exit(1);
 	}
+	
 	char buffer[1];
 	status = read(theSocket, buffer, 1);
 	if(status <= 0)
--- tcpip/server.c
+++ tcpip/server.c
@@ -7,16 +7,17 @@
 
 int main()
 {
-	struct sockaddr_in local;
-	local.sin_family = AF_INET;
-	local.sin_port = htons(7500);
-	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if(listenSocket < 0)
 	{
 		fprintf(stderr,"ошибка вызова функции socket\n");
 		exit(1);
 	}
+
+	struct sockaddr_in local;
+	local.sin_family = AF_INET;
+	local.sin_port = htons(7500);
+	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	int status;
 	status = bind(listenSocket, (struct sockaddr *) &local, sizeof(local));
 	if(status < 0)
@@ -24,18 +25,21 @@ int main()
 		fprintf(stderr,"ошибка вызова функции bind\n");
 		exit(1);
 	}
+
 	status = listen(listenSocket, 5);
 	if(status)
 	{
 		fprintf(stderr,"ошибка вызова функции listen\n");
 		exit(1);
 	}
+
 	int theSocket = accept(listenSocket, NULL, NULL);
 	if(theSocket < 0)
 	{
 		fprintf(stderr,"ошибка вызова функции accept\n");
 		exit(1);
 	}
+
 	char buffer[1];
 	status = read(theSocket, buffer, 1);
 	if(status <= 0)
@@ -44,6 +48,7 @@ int main()
 		exit(1);
 	}
 	fprintf(stdout,"%c\n",buffer[0]);
+	
 	status = write(theSocket, "b", 1);
 	if(status <= 0)
 	{
